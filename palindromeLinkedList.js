1299da3482c7a3631ebd9541a271ea7514f5cf54
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2023-05-24 00:40:52 +0000

    Solved ransomNote and updated palindrome

--- palindromeLinkedList.js
+++ palindromeLinkedList.js
@@ -9,11 +9,57 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// var isPalindrome = function (head) {
+//   let arr = [];
+//   while (head != null) {
+//     arr.push(head.val);
+//     head = head.next;
+//   }
+//   return arr.join("") === arr.reverse().join("");
+// };
+
+// Faster and better memory management Solution
+
 var isPalindrome = function (head) {
   let arr = [];
   while (head != null) {
     arr.push(head.val);
     head = head.next;
   }
-  return arr.join("") === arr.reverse().join("");
+  // return arr.join('') === arr.reverse().join('')
+  let l = 0,
+    r = arr.length - 1;
+  let result = true;
+  while (l !== r && arr.length > 1 && l >= 0 && r >= 0) {
+    if (arr[l] === arr[r]) {
+      result = true;
+    } else {
+      result = false;
+      break;
+    }
+    l++;
+    r--;
+  }
+  return result;
 };
+
+// Even faster and better
+var isPalindrome = function (head) {
+  let stack = [];
+  let pointer = head;
+  while (pointer) {
+    stack.push(pointer.val);
+    pointer = pointer.next;
+  }
+  while (head) {
+    if (head.val !== stack.pop()) {
+      return false;
+    } else {
+      head = head.next;
+    }
+  }
+  return true;
+};
+
+// Even Better
+// I'm coming back for this
